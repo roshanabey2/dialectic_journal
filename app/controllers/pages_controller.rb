b18c87f9c824784fcd2cd8da60a22266a8da4302
Roshan Abeysekera <roshanabey2@gmail.com>
2025-05-06 13:01:26 +0100

    Styles and controllers updates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  # allow_unauthenticated_access only: %i[ index ]
+   allow_unauthenticated_access only: %i[ home dashboard ]
 
   def home
     render json: { message: "Welcome to Dialectic Journal" }
